bfe0e4d7f26fe43fbe962364ec5fa0c5b147912e
john7john <124849959+john7john@users.noreply.github.com>
2023-06-01 21:23:47 -0400

    f2

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,47 +6,51 @@ function init() {
     d3.json(url).then((data) => {
         console.log(`Data: ${data}`);
         
-        let names = data.names;        
-        names.forEach((name) => {          
-            
+        let names = data.names;
+        
+        names.forEach((name) => {            
             dropdownMenu.append("option").text(name).property("value", name);
-        });        
-        let name = names[0];     
+        });
+        
+        let name = names[0];
+        
         demo(name);
         bar(name);
         bubble(name);
         gauge(name);
     });
 }
+
+
 function demo(selectedValue) {
     
     d3.json(url).then((data) => {
-        console.log(`Data: ${data}`);
+        console.log(`Data: ${data}`);        
+        let metadata = data.metadata;      
         
-        let metadata = data.metadata;   
+        let filteredData = metadata.filter((meta) => meta.id == selectedValue);      
         
-        let filteredData = metadata.filter((meta) => meta.id == selectedValue);
-      
-        let obj = filteredData[0]
+        let obj = filteredData[0]        
         
         d3.select("#sample-metadata").html("");  
-                
-        let entries = Object.entries(obj);
-                       
+        
+        let entries = Object.entries(obj);        
+        
         entries.forEach(([key,value]) => {
             d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
-        });
-                console.log(entries);
+        });        
+        console.log(entries);
     });
   } 
 
 
-function bar(selectedValue) {    
+function bar(selectedValue) {
+    
     d3.json(url).then((data) => {
         console.log(`Data: ${data}`);        
-        let samples = data.samples;         
+        let samples = data.samples;        
         let filteredData = samples.filter((sample) => sample.id === selectedValue);        
-        let obj = filteredData[0];      
+        let obj = filteredData[0];        
         
         let trace = [{            
             x: obj.sample_values.slice(0,10).reverse(),
@@ -57,18 +61,16 @@ function bar(selectedValue) {
                 color: "rgb(166,172,237)"
             },
             orientation: "h"
-        }];
+        }];       
         
-        // Plotly 
         Plotly.newPlot("bar", trace);
     });
-}
-  
-//  bubble chart
-function bubble(selectedValue) {
-    
-    d3.json(url).then((data) => {        
-        let samples = data.samples; 
+}  
+
+function bubble(selectedValue) {    
+    d3.json(url).then((data) => {
+        
+        let samples = data.samples;   
          
         let filteredData = samples.filter((sample) => sample.id === selectedValue);   
         
@@ -84,14 +86,20 @@ function bubble(selectedValue) {
                 color: obj.otu_ids,
                 colorscale: "Sunset"
             }
-        }];   
+        }];    
         
         let layout = {
             xaxis: {title: "OTU ID"}
-        };   
+        };    
         
         Plotly.newPlot("bubble", trace, layout);
     });
+}
+ 
+function optionChanged(selectedValue) {
+    demo(selectedValue);
+    bar(selectedValue);
+    bubble(selectedValue);    
 }
 
 init();
